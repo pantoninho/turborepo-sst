39c4d391462ebb14c659fd6dd95243e0e961e007
Pedro Antoninho <pdn.antoninho@gmail.com>
2024-09-19 03:15:34 +0100

    feat: astro-webapp bg

--- apps/astro-webapp/tailwind.config.ts
+++ apps/astro-webapp/tailwind.config.ts
@@ -1,9 +1,34 @@
 import type { Config } from "tailwindcss";
 import uiConfig from "@neuronio/ui/tailwind.config";
+import plugin from 'tailwindcss/plugin'
 
 const config: Config = {
     content: ["./src/**/*.{tsx,astro}"],
     presets: [uiConfig],
+    theme: {
+        extend: {
+            fontFamily: {
+                'mono': ['"Roboto Mono"', 'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
+            },
+            textShadow: {
+                sm: '0 1px 2px var(--tw-shadow-color)',
+                DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+                lg: '0 8px 16px var(--tw-shadow-color)',
+            },
+        }
+    },
+    plugins: [
+        plugin(function ({ matchUtilities, theme }) {
+            matchUtilities(
+                {
+                    'text-shadow': (value) => ({
+                        textShadow: value,
+                    }),
+                },
+                { values: theme('textShadow') }
+            )
+        }),
+    ]
 };
 
 
